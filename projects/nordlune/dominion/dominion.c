5d63b35da6064163d6019b99755bc0fd50ad5dfb
Erik Nordlund <contact@eriknordlund.com>
2019-07-07 13:09:05 -0700

    refactored cardEffectMinion

--- projects/nordlune/dominion/dominion.c
+++ projects/nordlune/dominion/dominion.c
@@ -701,6 +701,57 @@ int cardEffectBaron(int choice1, struct gameState *state, int currentPlayer) {
 	
 }
 
+int cardEffectMinion(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer) {
+	//+1 action
+	state->numActions++;
+	
+	//discard card from hand
+	discardCard(handPos, currentPlayer, state, 0);
+	
+	if (choice1)		//+2 coins
+	{
+	  state->coins = state->coins + 2;
+	}
+	
+	else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+	{
+		//discard hand
+		while(numHandCards(state) > 0)
+	    {
+	      discardCard(handPos, currentPlayer, state, 0);
+	    }
+		
+		//draw 4
+		for (int i = 0; i < 4; i++)// initializing i here might not be the best (make it more global for the next for loop?)
+	    {
+	      drawCard(currentPlayer, state);
+	    }
+		
+		//other players discard hand and redraw if hand size > 4
+		for (int i = 0; i < state->numPlayers; i++)
+	    {
+	    	if (i != currentPlayer)
+			{
+				if ( state->handCount[i] > 4 )
+		    	{
+		    		//discard hand
+		    		while( state->handCount[i] > 0 )
+		    		{
+		    			discardCard(handPos, i, state, 0);
+		    		}
+					
+					//draw 4
+					for (int j = 0; j < 4; j++)
+					{
+						drawCard(i, state);
+					}
+				}
+			}
+	    }
+	}
+      return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -923,55 +974,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case minion:
-      //+1 action
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      if (choice1)		//+2 coins
-	{
-	  state->coins = state->coins + 2;
-	}
-			
-      else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-	{
-	  //discard hand
-	  while(numHandCards(state) > 0)
-	    {
-	      discardCard(handPos, currentPlayer, state, 0);
-	    }
-				
-	  //draw 4
-	  for (i = 0; i < 4; i++)
-	    {
-	      drawCard(currentPlayer, state);
-	    }
-				
-	  //other players discard hand and redraw if hand size > 4
-	  for (i = 0; i < state->numPlayers; i++)
-	    {
-	      if (i != currentPlayer)
-		{
-		  if ( state->handCount[i] > 4 )
-		    {
-		      //discard hand
-		      while( state->handCount[i] > 0 )
-			{
-			  discardCard(handPos, i, state, 0);
-			}
-							
-		      //draw 4
-		      for (j = 0; j < 4; j++)
-			{
-			  drawCard(i, state);
-			}
-		    }
-		}
-	    }
-				
-	}
-      return 0;
+    	return cardEffectMinion(choice1, choice2, state, handPos, currentPlayer);
 		
     case steward:
       if (choice1 == 1)
