97b3629f8f4f004ec7fc56f5598fd5c202c96513
Erik Nordlund <contact@eriknordlund.com>
2019-07-21 22:01:54 -0700

    created test for initializeGame

---
+++ projects/nordlune/dominion/cardtest1.c
@@ -0,0 +1,36 @@
+// Unit test for initializeGame
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <assert.h>
+#include "dominion_helpers.h"
+
+
+void testInitializeGame() {
+     for (int numPlayers = 2; numPlayers <= MAX_PLAYERS; ++numPlayers) {
+          // testing for all possible numbers of players
+          struct gameState *state;
+
+          int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+
+          memset(&state, 23, sizeof(struct gameState));   // clear the game state
+
+          int game = initializeGame(numPlayers, k, seed, &state); // initialize a new game
+          
+          // now that the game is initialized, check state
+          assert(state->numPlayers == numPlayers);
+          
+          assert(state->numActions == 1);
+          
+          assert(state->numBuys == 1);
+          
+          for (int player = 0; player < numPlayers; ++player) {
+               assert(state->handCount[player] == 5);
+          }
+     }
+}
+
+int main() {
+    testInitializeGame();
+    return 0;
+}
