bc656fef76bbdef9464a087dc2943e2586da0338
Erik Nordlund <contact@eriknordlund.com>
2019-07-21 23:12:00 -0700

    created test for getWinners

---
+++ projects/nordlune/dominion/cardtest4.c
@@ -0,0 +1,32 @@
+// Unit test for getWinners
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <assert.h>
+#include "dominion_helpers.h"
+
+
+void testGetWinners() {
+     for (int numPlayers = 2; numPlayers <= MAX_PLAYERS; ++numPlayers) {
+          // testing for all possible numbers of players
+          struct gameState *state;
+
+          int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+
+          memset(&state, 23, sizeof(struct gameState));   // clear the game state
+
+          int game = initializeGame(numPlayers, k, seed, &state); // initialize a new game
+          
+          int initialTurn = state->whoseTurn;
+          
+          int winners = getWinners(int players[MAX_PLAYERS], &state);
+          
+          
+          assert(winners < numPlayers);
+     }
+}
+
+int main() {
+    testGetWinners();
+    return 0;
+}
