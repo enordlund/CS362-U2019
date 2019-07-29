659bd18b5edb84cf8d82fe935458852c4cd224d8
Erik Nordlund <contact@eriknordlund.com>
2019-07-28 19:56:07 -0700

    added refactored cardEffect functions to header

--- projects/nordlune/dominion/dominion_helpers.h
+++ projects/nordlune/dominion/dominion_helpers.h
@@ -9,6 +9,13 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+
+int cardEffectBaron(int choice1, struct gameState *state, int currentPlayer);
+int cardEffectMinion(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardEffectAmbassador(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int cardEffectTribute(struct gameState *state, int currentPlayer, int nextPlayer);
+int cardEffectMine(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
